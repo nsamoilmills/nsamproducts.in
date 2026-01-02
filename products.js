e36e7fcb555f6305a5ee884668d26b036dfb5143
nsamoilmills <nsamoilmills@gmail.com>
2026-01-02 19:09:54 +0530

    Update product images in products.js

--- products.js
+++ products.js
@@ -9,7 +9,7 @@ window.PRODUCTS = [
     price: 300,
     category: "Oils",
     unit: "1 L",
-    image: "assets/sample.png"
+    image: "assets/product1.png"
   },
   {
     sku: "HRB-SEMB-100",
@@ -18,7 +18,7 @@ window.PRODUCTS = [
     price: 100,
     category: "Herbal Powders",
     unit: "100 g",
-    image: "assets/sample.png"
+    image: "assets/product1.png"
   }
 ];
 
